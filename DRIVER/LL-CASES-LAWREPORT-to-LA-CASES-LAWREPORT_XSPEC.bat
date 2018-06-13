c4fcd047156b7c82f51f3bb4b9ab9853a757f540
sharmac2 <chaitanya.sharma@lexisnexis.com>
2018-06-13 15:27:45 +0530

    Signed-off-by: sharmac2 <chaitanya.sharma@lexisnexis.com>

--- DRIVER/LL-CASES-LAWREPORT-to-LA-CASES-LAWREPORT_XSPEC.bat
+++ DRIVER/LL-CASES-LAWREPORT-to-LA-CASES-LAWREPORT_XSPEC.bat
@@ -1,2 +1,2 @@
 set SAXON_CP=C:\saxon\saxon9he.jar
-\bin\xspec.bat -j \DRIVER\LL-CASES-LAWREPORT-to-LA-CASES-LAWREPORT_XSPEC.xspec
+\bin\xspec.bat -j \UK-ICS\DRIVER\LL-CASES-LAWREPORT-to-LA-CASES-LAWREPORT_XSPEC.xspec
